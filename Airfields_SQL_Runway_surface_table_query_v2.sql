6d21b043c5f88dc84363b4d7f239e5ce6364cf71
benniehana111 <103535389+benniehana111@users.noreply.github.com>
2022-09-27 17:10:07 -0400

    Delete Airfields_SQL_Runway_surface_table_query_v2.sql

--- Airfields_SQL_Runway_surface_table_query_v2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE runway_surface (
-	GPS_Code VARCHAR(4) NOT NULL,
-	surface SMALLINT,
-	FOREIGN KEY (GPS_Code) REFERENCES airfields (GPS_Code),
-	PRIMARY KEY (GPS_Code),
-	UNIQUE (GPS_Code)
-);
